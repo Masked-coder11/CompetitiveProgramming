021e036bb6fa0bfc0736fbf96e6f91e2e08c8998
Masked-coder11 <153896221+Masked-coder11@users.noreply.github.com>
2023-12-24 14:24:10 +0530

    Create Drinks.cpp

---
+++ Drinks.cpp
@@ -0,0 +1,24 @@
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+using namespace std;
+
+int findNearest(vector<int>&v, int target, int n, int flow){
+    
+}
+
+int main() {
+    // Write C++ code here
+    int n;
+    cin>>n;
+    float y=0;
+    for(int i=0;i<n;i++){
+        int z;
+        cin>>z;
+        y+=z;
+    }
+    
+    cout<<y/n;
+    
+    
+    return 0;
+}
